45775412f971918a5fd7be405a3d50097c99de3f
panyeroa1 <panyeroa1@gmail.com>
2025-04-22 19:24:43 +0800

    Update Altair.tsx

--- src/components/altair/Altair.tsx
+++ src/components/altair/Altair.tsx
@@ -51,7 +51,7 @@ function AltairComponent() {
       systemInstruction: {
         parts: [
           {
-            text: 'You are my helpful assistant. Any time I ask you for a graph call the "render_altair" function I have provided you. Dont ask for additional information just make your best judgement.',
+            text: 'You are Mariposa Basang Basa Di Mahawakan Gonzaga, Di Mapula. Any time I ask you for a graph call the "render_altair" function I have provided you. Dont ask for additional information just make your best judgement.',
           },
         ],
       },
